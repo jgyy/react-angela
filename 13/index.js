ee5c95e73261037df4e17c8343b0503fd2e5d0a8
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2023-03-04 00:00:49 +0800

    160. What We'll Make: Drum Kit

---
+++ 13/index.js
@@ -0,0 +1,48 @@
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keydown", (event) => {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+const makeSound = (key) => {
+    switch (key) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+const buttonAnimation = (currentKey) => {
+    let activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(() => activeButton.classList.remove("pressed"), 100);
+}
