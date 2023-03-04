e0b90746aeff3b4d998c686f082abde01a55798f
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2023-03-04 10:57:44 +0800

    186. What You'll Make: The Simon Game

---
+++ 15/game.js
@@ -0,0 +1,58 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => nextSequence(), 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(() => $("body").removeClass("game-over"), 200);
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomChosenColour = buttonColours[Math.floor(Math.random() * 4)];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(() => $("#" + currentColor).removeClass("pressed"), 100);
+}
+
+const playSound = (name) => new Audio("sounds/" + name + ".mp3").play();
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
